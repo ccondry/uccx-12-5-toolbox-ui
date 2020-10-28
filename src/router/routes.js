0cb3029b2c0b695d2a647d0cc620f47a431048ea
Coty Condry <ccondry@cisco.com>
2020-10-28 05:28:01 -0500

    update route paths

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,11 @@
 const routes = [{
   // catch-all to redirect to home view if no route matched
   path: '*',
-  redirect: '/'
+  redirect: '/uccx-12-5/'
 }, {
   // the home page
   name: 'Home',
-  path: '/',
+  path: '/uccx-12-5/',
   component: () => import(`../views/home.vue`)
 }]
 export default routes
